0201ef7b5d8451c055e371586d56a0dd1a6ef508
Patrik Márton <marpataa@yahoo.com>
2020-08-31 11:29:34 +0200

    Initial commit

---
+++ src/main/java/com/example/awstutorial/demo/config/DynamoDBConfig.java
@@ -0,0 +1,24 @@
+package com.example.awstutorial.demo.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDBConfig {
+
+    public DynamoDBMapper mapper(){
+        return new DynamoDBMapper(amazonDynamoDBConfig());
+    }
+
+    private AmazonDynamoDB amazonDynamoDBConfig() {
+        return AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("dynamodb.eu-west-2.amazonaws.com","u-west-2"))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("TODO","TODO")))
+                .build();
+    }
+}
